af72f5056317476fa1cc5082dbf5311bcb594876
Yizo-Yizo <56066066+Yizo-Yizo@users.noreply.github.com>
2020-03-12 11:09:13 +0200

    mvvm achitecture

---
+++ FYM/FYM/FYM/Services/RestClient.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FYM.Services
+{
+    class RestClient<T>
+    {
+        private const string LoginWebServiceUrl = "";
+
+        public async Task<bool> CheckLogin(string email, string password)
+        {
+            var httpClient = new HttpClient();
+
+            var credentials = new { email = email, password = password };
+
+            var stringContent = JsonConvert.SerializeObject(credentials);
+
+            var content = new StringContent(stringContent, Encoding.UTF8, "application/json");
+
+            var response = await httpClient.PostAsync(LoginWebServiceUrl, content);
+
+            if (response.StatusCode == HttpStatusCode.NotFound)
+                return false;
+
+            if (response.StatusCode == HttpStatusCode.OK)
+                return true;
+
+            return false;
+        }
+    }
+}
