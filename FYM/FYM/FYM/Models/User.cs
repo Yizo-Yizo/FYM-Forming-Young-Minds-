7c6e3e76c707471eab0ad36280a0d7d3fcca8c43
Yizo-Yizo <56066066+Yizo-Yizo@users.noreply.github.com>
2020-03-12 11:48:51 +0200

    name

--- FYM/FYM/FYM/Models/User.cs
+++ FYM/FYM/FYM/Models/User.cs
@@ -1,10 +1,24 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace FYM.Models
 {
-    class User
+    public class User
     {
+        [PrimaryKey, AutoIncrement]
+
+        public int ID { get; set; }
+
+        public string FirstNames { get; set; }
+
+        public string LastName { get; set; }
+
+        public int PhoneNumber { get; set; }
+
+        public string Password { get; set; }
+
+        public string ConfirmPassword { get; set; }
     }
 }
